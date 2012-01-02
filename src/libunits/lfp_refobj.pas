f178540df40b02ae77e0142029a6bb34ff6ae736
Yury Benesh <ybxsoft@tut.by>
2012-01-02 21:37:58 +0300

    Cleaned interface, made more consistent.

--- src/libunits/lfp_refobj.pas
+++ src/libunits/lfp_refobj.pas
@@ -15,12 +15,11 @@ type
   TRefObserver = class;
 
   /// base for your interfaces, so that cast to TRefObject can always be made.
-  IRefObject = interface
-    function retain : TRefObject;
+  IRefObject = interface ['IRefObject']
+    function retain : IRefObject;
     function release : boolean; // returns true if pointer must be set to nil
     function createRefObserver : TRefObserver;
-    function asTRefObject : TRefObject;
-    function clone : TRefObject;
+    function asTObject : TObject;
   end;
 
   /// sets pointer to TRefObject to nil when reference counter = 0
@@ -61,23 +60,25 @@ type
 
     public
       { IRefObject }
-	  function retain : TRefObject;
+	  function retain : IRefObject;
 	  function release : boolean; // returns true if object must be disposed
 	  function createRefObserver : TRefObserver;
-      function asTRefObject : TRefObject;
-      function clone : TRefObject;
+      function asTObject : TObject;
   end;
 
 
-  function safeRetain(o : IRefObject) : TRefObject;inline;
-  procedure safeRelease(o : IRefObject; var vo);
-  procedure safeRelease(o : TRefObject; var vo);
+  function safeRetain(o : IRefObject) : IRefObject;inline;
+
+  /// calls release (if not nil) and assigns nil to the variable
+  /// fpc 2.6 does not allow to pass derived classes as var arguments,
+  /// so we have to duplicate as untyped.
+  procedure safeRelease(o : IRefObject; var vartonil);
 
 implementation
 uses sysutils;
 const refObjectMagic = $13254769;
 
-function safeRetain(o: IRefObject): TRefObject;
+function safeRetain(o: IRefObject): IRefObject;
 begin
   if o <> nil then
      exit(o.retain);
@@ -85,23 +86,15 @@ begin
   result := nil;
 end;
 
-procedure safeRelease(o: IRefObject; var vo);
+procedure safeRelease(o: IRefObject; var vartonil);
 begin
   if o <> nil then
      begin
        if o.release then
-          pointer(vo) := nil;
+          pointer(vartonil) := nil;
      end;
 end;
 
-procedure safeRelease(o: TRefObject; var vo);
-begin
-  if o <> nil then
-     begin
-       if o.release then
-          pointer(vo) := nil;
-     end;
-end;
 
 {
 procedure safeRelease(var o);
@@ -138,7 +131,7 @@ begin
   inherited destroy;
 end;
 
-function TRefObject.retain: TRefObject;
+function TRefObject.retain: IRefObject;
 begin
   refstruct^.incStrong;
   result := self;
@@ -161,14 +154,9 @@ begin
   result := TRefObserver.create(refstruct);
 end;
 
-function TRefObject.asTRefObject: TRefObject;
+function TRefObject.asTObject: TObject;
 begin
   result := self;
-end;
-
-function TRefObject.clone: TRefObject;
-begin
-  result := nil; // not supported
 end;
 
 { TRefObserver }
